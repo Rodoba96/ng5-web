92fc8a9967ee79b928d90e23abb11c3e2112b80f
Rodoba96 <A01702398@itesm.mx>
2021-09-27 10:42:38 -0500

    modify package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserDynamicTestingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
